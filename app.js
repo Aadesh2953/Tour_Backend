de78c94d381a4975081945004bb5dea385c7ae0d
Aadesh <shuklaaadesh0@gmail.com>
2024-11-15 12:08:47 +0530

    Added Config File

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express, { json } from "express";
 import { tourRouter } from "./Routes/TourRoute.js";
 import { userRouter } from "./Routes/UserRoute.js";
+import { connectToDb } from "./index.js";
+import dotenv from 'dotenv'
+dotenv.config({path:'./config.env'})
 const app = express();
+connectToDb()
 app.use(express.json({ limit: "16kb" }));
 app.use((req, res, next) => {
   console.log("Hello World");
