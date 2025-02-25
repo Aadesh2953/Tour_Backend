2517eeda0c64e10af5dc589d95c4edc236219613
Aadesh <shuklaaadesh0@gmail.com>
2025-02-25 10:46:47 +0530

    Added Email Class

--- utils/email.js
+++ utils/email.js
@@ -1,4 +1,44 @@
 import nodemailer from 'nodemailer';
+import pug from 'pug';
+const __dirname = path.dirname(__filename);
+export class Email{
+  constructor(user,url)
+  {
+     this.name=user.split(' ')[0];
+     this.sendTo=user.email
+     this.from=process.env.EMAIL
+     this.url=url
+  }
+  createTransport()
+  {
+    return this.nodemailer.createTransport({
+        host:'smtp.gmail.com',
+        port:587,
+        secure:false,
+        servie:"gmail",
+        auth:{
+          user:process.env.EMAIL_USER,
+          pass:process.env.EMAIL_PASSWORD,
+        }
+    })
+  }
+  async sendMail(template,subject)
+  {
+    const html=pug.renderFile(`../views/${template}.pug`,{name:this.name,sendTo:this.sendTo,subject})
+    const mailOptions={
+        from:process.env.EMAIL,
+        to:this.sendTo,
+        subject,
+        // text:htm,Implement Later
+        html
+    }
+    await this.createTransport().sendMail(mailOptions);
+  }
+  async sendWelcome()
+  {
+   await this.sendMail('Welcome','Welcome To TourQuest')
+  }
+}
 export const sendEmail=async(options)=>
 {
 
