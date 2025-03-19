0e4e4963175dace562b7bb07bcccae1660b8061d
Aadesh <shuklaaadesh0@gmail.com>
2025-03-19 11:15:44 +0530

    Final Minor Changes

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 import { connectToDb } from './db/index.js';
 dotenv.config({ path: './config.env' });
 connectToDb().then(()=>{
-  app.listen(process.env.PORT||5000,() => {
-    console.log('port',process.env.PORT);
-    console.log('as',500);
-    console.log(`App listening on http://localhost:5000`);
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`App listening on port ${PORT}`);
   });
+  
 }).catch((err)=>
 {
   console.log('err',err)
