bd064a3d91f6c6f6de5a01a336bf596269e389e2
Aadesh <shuklaaadesh0@gmail.com>
2025-03-19 11:11:52 +0530

    Minor

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import { connectToDb } from './db/index.js';
 dotenv.config({ path: './config.env' });
 connectToDb().then(()=>{
   app.listen(process.env.PORT||5000,() => {
+    console.log('port',process.env.PORT);
+    console.log('as',500);
     console.log(`App listening on http://localhost:5000`);
   });
 }).catch((err)=>
